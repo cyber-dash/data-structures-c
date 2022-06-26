93140f248bdd9c3643101965a68a2bcce541f91a
cyberdash_yuan <cyberdash@163.com>
2022-06-26 22:42:46 +0800

    栈代码调整

--- 3_Stack_Queue/Stack/src/test.c
+++ 3_Stack_Queue/Stack/src/test.c
@@ -3,7 +3,7 @@
 //
 
 #include "test.h"
-#include "stack.h"
+#include "seq_stack.h"
 #include "stdlib.h"
 #include "stdio.h"
 
@@ -14,19 +14,19 @@ void TestStackInsert() {
     printf("|                        Test Insert                        |\n");
     printf("|                          测试栈插入                         |\n");
 
-    SqStack *sqStack = (SqStack*)malloc(sizeof(SqStack));
-    Status res = InitStack(sqStack);
+    seq_stack_t *sqStack = (seq_stack_t*)malloc(sizeof(seq_stack_t));
+    Status res = SeqStackInit(sqStack);
     if (res != OK) {
         printf("初始化栈失败");
     }
 
-    Push(sqStack, 1);
-    Push(sqStack, 2);
-    Push(sqStack, 3);
-    Push(sqStack, 4);
-    Push(sqStack, 5);
+    SeqStackPush(sqStack, 1);
+    SeqStackPush(sqStack, 2);
+    SeqStackPush(sqStack, 3);
+    SeqStackPush(sqStack, 4);
+    SeqStackPush(sqStack, 5);
 
-    PrintStack(sqStack);
+    SeqStackPrint(sqStack);
 
     printf("|-----------------------------------------------------------|\n\n");
 }
@@ -35,24 +35,24 @@ void TestStackInsert() {
 void TestClearStack() {
     printf("\n");
     printf("|------------------------ CyberDash ------------------------|\n");
-    printf("|                      Test ClearStack                      |\n");
+    printf("|                      Test SeqStackClear                      |\n");
     printf("|                          测试清除栈                         |\n");
 
-    SqStack sqStack;
-    Status res = InitStack(&sqStack);
+    seq_stack_t sqStack;
+    Status res = SeqStackInit(&sqStack);
     if (res != OK) {
         printf("初始化栈失败");
     }
 
-    Push(&sqStack, 1);
-    Push(&sqStack, 2);
-    Push(&sqStack, 3);
-    Push(&sqStack, 4);
-    Push(&sqStack, 5);
+    SeqStackPush(&sqStack, 1);
+    SeqStackPush(&sqStack, 2);
+    SeqStackPush(&sqStack, 3);
+    SeqStackPush(&sqStack, 4);
+    SeqStackPush(&sqStack, 5);
 
-    res = ClearStack(&sqStack);
+    res = SeqStackClear(&sqStack);
     if (res == OK) {
-        if (StackEmpty(sqStack) == OK) {
+        if (SeqStackIsEmpty(sqStack) == OK) {
             printf("清除栈成功\n");
         }
     }
@@ -67,19 +67,19 @@ void TestStackLength() {
     printf("|                        Test Length                        |\n");
     printf("|                          测试栈长度                         |\n");
 
-    SqStack sqStack;
-    Status res = InitStack(&sqStack);
+    seq_stack_t sqStack;
+    Status res = SeqStackInit(&sqStack);
     if (res != OK) {
         printf("初始化栈失败");
     }
 
-    Push(&sqStack, 1);
-    Push(&sqStack, 2);
-    Push(&sqStack, 3);
-    Push(&sqStack, 4);
-    Push(&sqStack, 5);
+    SeqStackPush(&sqStack, 1);
+    SeqStackPush(&sqStack, 2);
+    SeqStackPush(&sqStack, 3);
+    SeqStackPush(&sqStack, 4);
+    SeqStackPush(&sqStack, 5);
 
-    printf("栈长度: %d\n", StackLength(sqStack));
+    printf("栈长度: %d\n", SeqStackSize(sqStack));
 
     printf("|-----------------------------------------------------------|\n\n");
 }
@@ -88,24 +88,24 @@ void TestStackLength() {
 void TestGetTop() {
     printf("\n");
     printf("|------------------------ CyberDash ------------------------|\n");
-    printf("|                        Test GetTop                        |\n");
+    printf("|                        Test SeqStackTop                        |\n");
     printf("|                         测试获取栈顶                        |\n");
 
-    SqStack sqStack;
-    Status res = InitStack(&sqStack);
+    seq_stack_t sqStack;
+    Status res = SeqStackInit(&sqStack);
     if (res != OK) {
         printf("初始化栈失败");
     }
 
-    SElemType top;
+    STACK_ELEM top;
 
-    Push(&sqStack, 1);
-    Push(&sqStack, 2);
-    Push(&sqStack, 3);
-    Push(&sqStack, 4);
-    Push(&sqStack, 5);
+    SeqStackPush(&sqStack, 1);
+    SeqStackPush(&sqStack, 2);
+    SeqStackPush(&sqStack, 3);
+    SeqStackPush(&sqStack, 4);
+    SeqStackPush(&sqStack, 5);
 
-    GetTop(sqStack, &top);
+    SeqStackTop(sqStack, &top);
 
     printf("栈顶: %d\n", top);
 
